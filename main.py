2f693cdbd15ca222779dedd06bb33ddd3d9bdf6b
Snegovsky <nik.snegovskyvlad@gmail.com>
2021-08-13 20:45:56 +0300

    ha

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-if __name__ == '__main__':
-    print('Hello AWS!')
+import json
+
+def lambda_handler():
+    message = 'Hello AWS!'
+    print(message)
+    return {
+        'statusCode': 200,
+        'body': json.dumps(message)
+    }
+
+lambda_handler()
