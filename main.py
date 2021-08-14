aebf4c124e2ad5305adcf9a2729d48340636583b
VladSnegovsky <50777219+VladSnegovsky@users.noreply.github.com>
2021-08-14 19:36:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import json
 
-def lambda_handler():
-    message = 'Hello World!'
+def lambda_handler(event, context):
+    message = "Hello AWS!"
     print(message)
     return {
         'statusCode': 200,
         'body': json.dumps(message)
     }
 
-lambda_handler()
-# Test
+lambda_handler(None, None)
