77a7bf6c652238da4d3a1cdf45bc54b61f3cb372
VladSnegovsky <50777219+VladSnegovsky@users.noreply.github.com>
2021-08-16 17:48:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import json
 
 def lambda_handler(event, context):
-    message = "<!DOCTYPE html> <html> <head> <meta charset = \"UTF-8\" > </head> <body> Проверка 1, Проверка 2, Проверка 3 </body> </html>"
+    message = "Проверка 1, Проверка 2, Проверка 3"
     print(message)
     return {
         'statusCode': 200,
-        'body': json.dumps(message)
+        'body': json.dumps(message, ensure_ascii=False)
     }
 
 lambda_handler(None, None)
