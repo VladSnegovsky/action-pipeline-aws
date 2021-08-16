69158f4246b9eaffea89a852b10f06d61df525e7
VladSnegovsky <50777219+VladSnegovsky@users.noreply.github.com>
2021-08-16 18:34:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
+import requests
 
 def lambda_handler(event, context):
+    url = 'https://wm3ce6ayrl.execute-api.us-east-2.amazonaws.com/default/python-aws'
+    response = requests.get(url, params=(), headers=())
     message = "Проверка 1, Проверка 2, Проверка 3"
     print(message)
     return {
