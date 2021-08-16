41076e3e80f85e9e8bccf0653618bb1545c1c0fb
VladSnegovsky <50777219+VladSnegovsky@users.noreply.github.com>
2021-08-16 18:31:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import json
+import requests
+
+def get_last_list():
+    url = 'https://wm3ce6ayrl.execute-api.us-east-2.amazonaws.com/default/python-aws'
+    response = requests.get(url, params=(), headers=())
+    response = str(response.content.decode('utf8'))
+    response = response[1:-1]
+    list = response.split(",")
+    return list
+
+
+def list_to_str(list):
+    return '\"' + str(list[0]) + ', ' + str(list[1]) + ', ' + str(list[2]) + '\"'
+
 
 def lambda_handler(event, context):
-    message = "Проверка 1, Проверка 2, Проверка 3"
+    message = list_to_str(get_last_list())
     print(message)
     return {
         'statusCode': 200,
